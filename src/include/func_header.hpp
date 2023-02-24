cd0f61450cc88e6f296ad10fb3fe9438d62ffff3
Mediocrius (Vano) <vano.khazalashvili@gmail.com>
2023-02-24 17:25:46 +0400

    nothing special 03

--- src/include/func_header.hpp
+++ src/include/func_header.hpp
@@ -5,24 +5,29 @@
 #define FunctionHeader func_header
 
 void variable_type_sizes(){
-    int64_t int64_t{};
-    uint64_t uint64_t{};
-    int32_t int32_t{};
-    uint32_t uint32_t{};
-    int16_t int16_t{};
-    uint16_t uint16_t{};
     int8_t int8_t{};
+    int16_t int16_t{};
+    int32_t int32_t{};
+    int64_t int64_t{};
     uint8_t uint8_t{};
+    uint16_t uint16_t{};
+    uint32_t uint32_t{};
+    uint64_t uint64_t{};
+
+    __int8 _int8{};
+    __int16 _int16{};
+    __int32 _int32{};
+    __int64 _int64{};
+
+
 
     char16_t char16t[] = { };
     char32_t char32t[] = { };
     unsigned char unsignedChar[] = { }; 
     char _char[] = { };
     wchar_t wChar_t[] = { }; 
-    __int8 _int8{};
-    __int16 _int16{};
-    __int32 _int32{};
-    __int64 _int64{};
+    
+
 
     cout<<"size of int64_t "<<sizeof(int64_t)<<" bytes\n";
     cout<<"size of uint64_t "<<sizeof(uint64_t)<<" bytes\n";
@@ -34,36 +39,43 @@ void variable_type_sizes(){
     cout<<"size of uint8_t " <<sizeof(uint8_t)<<" bytes\n";
     
     cout<<"size of __int8 " <<sizeof(_int8)<<" bytes\n";
+    cout<<"size of __int16 "<<sizeof(_int16)<<" bytes\n";
     cout<<"size of __int32 "<<sizeof(_int32)<<" bytes\n";
     cout<<"size of __int64 "<<sizeof(_int64)<<" bytes\n";
-    cout<<"size of __int16 "<<sizeof(_int16)<<" bytes\n";
-    cout<<"size of __int " <<sizeof(uint8_t)<<" bytes\n";
-    cout<<"size of __int " <<sizeof(uint8_t)<<" bytes\n";
-    cout<<"size of __int " <<sizeof(uint8_t)<<" bytes\n";
-    cout<<"size of __int " <<sizeof(uint8_t)<<" bytes\n";
-    cout<<"size of __int " <<sizeof(uint8_t)<<" bytes\n";
-    cout<<"size of __int " <<sizeof(uint8_t)<<" bytes\n";
+    cout<<"size of " <<sizeof(char16_t)<<" bytes\n";
+    cout<<"size of " <<sizeof(char32_t)<<" bytes\n";
+    cout<<"size of " <<sizeof(unsignedChar)<<" bytes\n";
+    cout<<"size of " <<sizeof(_char)<<" bytes\n";
+    cout<<"size of " <<sizeof(wChar_t)<<" bytes\n";
+    //cout<<"size of " <<sizeof()<<" bytes\n";
 }
 void sum_of_natural_numbers(){
     int32_t sum{};
     for(int i = 0; i<10; i++){
         sum = sum + i;
     }
-    cout<<sum;
+    cout<<"Sum of Natural Numbers: "<<sum;
 }
-void display_specific_amount_of_natural_numbers(){
-    int32_t x{}, j{};
-    int32_t natural_numbers_array[10] = {1,2,3,4,5,6,7,8,9,10};
-    
-    if(x > 0 || x <= 10){
-        cout<<"how many natural numbers should we display? enter number now: ";
-        cin>>x;
-        for(int j = 0; j < x; j++){
+void display_specific_amount_of_natural_numbers_and_their_sum(){
+    int x{}, j{}, i{};
+    int natural_numbers_array[10] = {1,2,3,4,5,6,7,8,9,10};
+    cout<<"how many natural numbers should we display? ";
+    cin>>x;
+
+    if(x > 0 && x <= 10){
+        for(j; j < x; j++ && std::isdigit(x) == true){
             cout<<natural_numbers_array[j]<<" ";
+            }
         }
+    else if(std::isdigit(x) == false){
+        cout<<"wrong. enter numbers between 1 and 10 ";
+        display_specific_amount_of_natural_numbers_and_their_sum();
     }
-    else{
-        cout<<"\nwrong number.\n";
+    for(i; i < x; i++){
+        __int32 sum{};
+        sum = sum += x;
     }
+        cout<<"\n"<<x<<"\n";
 }
+bool range_checker(); //check if integer and return true if it is within 10, 100 or 200.
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,9 @@ int main(int argc, char** argv){
     cout<<"\n\n";
     sum_of_natural_numbers();
     cout<<"\n\n";
-    display_specific_amount_of_natural_numbers();
+    display_specific_amount_of_natural_numbers_and_their_sum();
     cout<<"\n\n";
 
     return 0;
+    
 }
