59db73a0cbd3668a89c7e7eea26f923818bfaf68
Mediocrius <vano.khazalashvili@hotmail.com>
2023-03-01 23:54:03 +0400

    base-desktop-pc commit

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,20 @@ warning: if you want to add stuff document it well.
 #include "include/header.hpp"
 #include "include/func_header.hpp"
 
-int main(){  
+struct Person{      
+    char name[64];  //struct used for pointer usage example.
+    int age;
+};
+/*function updates the age of a Person structure. but its out of scope. to get around it we pass struct by reference, so that now the pointer to the structure is in the scope of this function*/
+void updateStruct(struct Person *p, int age){
+    p->age = age;
+}
+int main(int argc, char **argv){  
+    struct Person lowlevellearning;         //pointer
+    updateStruct(&lowlevellearning, 100);   //example
+    //cout<<"\n"<<lowlevellearning.age<<"\n\n";
+
+    
     dataTypes vartypesize;
     vartypesize.printTypeSizes();
     cout<<"\n\n";
